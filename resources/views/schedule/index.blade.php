987660e9abbf28ccedf154728f1e564d6b29bd83
Kendry Ortiz <ortizkendry95@gmail.com>
2020-05-02 12:26:13 -0500

    Validaciones en vista de horarios para usuarios especiales

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -11,24 +11,29 @@
 
       <div class="col-md-12">
 
-    		@if((!empty($toDay->inicio) && !empty($toDay->fin)) || (!isset($toDay->inicio) && !isset($toDay->fin)))
+    		@if (\Helper::operatorsRecoverSecu(session()->get('usuario')->Nombre))
+				<span class="label label-danger">No olvides iniciar y cerrar día con tu usuario real</span>
+				{{-- <div class="alert alert-danger">No olvides iniciar y cerrar día con tu usuario real</div> --}}
+			@else
+				@if((!empty($toDay->inicio) && !empty($toDay->fin)) || (!isset($toDay->inicio) && !isset($toDay->fin)))
 
-          <form action="{{ url('horario') }}" method="post">
-            {{ csrf_field() }}
-            <button type="submit" class="text-center btn btn-success" title="Iniciar día"> ¡Iniciar día!</button>
-          </form>
-
-        	@endif
-
-    		@if(!empty($toDay->ID) && (is_null($toDay->fin)))
-
-				<form action="{{ url('horario',[$toDay->ID]) }}" method="POST">
-					<input type="hidden" name="_method" value="PUT">
-					{{ csrf_field() }}
-					<button type="submit" class="text-center btn btn-danger" title="Iniciar día"> ¡Finalizar día!</button><em class="text-muted"> Iniciado a las {{ date('H:i', strtotime($toDay->inicio)) }}</em>
+				<form action="{{ url('horario') }}" method="post">
+				{{ csrf_field() }}
+				<button type="submit" class="text-center btn btn-success" title="Iniciar día"> ¡Iniciar día!</button>
 				</form>
 
-        	@endif
+				@endif
+
+				@if(!empty($toDay->ID) && (is_null($toDay->fin)))
+
+					<form action="{{ url('horario',[$toDay->ID]) }}" method="POST">
+						<input type="hidden" name="_method" value="PUT">
+						{{ csrf_field() }}
+						<button type="submit" class="text-center btn btn-danger" title="Iniciar día"> ¡Finalizar día!</button><em class="text-muted"> Iniciado a las {{ date('H:i', strtotime($toDay->inicio)) }}</em>
+					</form>
+
+				@endif
+			@endif
 
       </div>
 
@@ -36,71 +41,75 @@
 
     <div class="row">
 
-      <div class="col-md-6">
+      @if (!\Helper::operatorsRecoverSecu(session()->get('usuario')->Nombre))
+	  <div class="col-md-6">
 
-				<h4>Listado por día</h4>
+		<h4>Listado por día</h4>
 
-    		@if(count($shedulesDay) > 0)
+			@if(count($shedulesDay) > 0)
 
 					<div class="table-responsive">
-						<table border="0" align="center" cellpadding="0" cellspacing="5" class="table table-striped">
-							<thead>
-								<tr>
-									<th>Día</th>
-									<th>Inicio</th>
-									<th>Fin</th>
-									<th>Total</th>
-								</tr>
-							</thead>
-							<tbody>
-								@foreach($shedulesDay as $shedule)
+							<table border="0" align="center" cellpadding="0" cellspacing="5" class="table table-striped">
+								<thead>
 									<tr>
-
-										<td>{{ date("d-M", strtotime($shedule->Day)) }}</td>
-										<td>{{ date("H:i", strtotime($shedule->inicio)) }}</td>
-										<td>{{ date("H:i", strtotime($shedule->fin)) }}</td>
-										<td>{{ number_format((float)$shedule->Q_horas, 2, ',', '') }}</td>
-
+										<th>Día</th>
+										<th>Inicio</th>
+										<th>Fin</th>
+										<th>Total</th>
 									</tr>
-								@endforeach
-							</tbody>
-						</table>
-					</div>
+								</thead>
+								<tbody>
+									@foreach($shedulesDay as $shedule)
+										<tr>
 
-        @endif
+											<td>{{ date("d-M", strtotime($shedule->Day)) }}</td>
+											<td>{{ date("H:i", strtotime($shedule->inicio)) }}</td>
+											<td>{{ date("H:i", strtotime($shedule->fin)) }}</td>
+											<td>{{ number_format((float)$shedule->Q_horas, 2, ',', '') }}</td>
 
-      </div>
+										</tr>
+									@endforeach
+								</tbody>
+							</table>
+						</div>
 
-      <div class="col-md-5 col-md-offset-1">
-				<h4>Listado por mes</h4>
+			@endif
+
+		</div>
+	  @endif
+
+      @if (!\Helper::operatorsRecoverSecu(session()->get('usuario')->Nombre))
+	  <div class="col-md-5 col-md-offset-1">
+			<h4>Listado por mes</h4>
 				@if(count($shedulesMonth) > 0)
 
-					<div class="table-responsive">
-						<table border="0" align="center" cellpadding="0" cellspacing="5" class="table table-striped">
-							<thead>
+				<div class="table-responsive">
+					<table border="0" align="center" cellpadding="0" cellspacing="5" class="table table-striped">
+						<thead>
+							<tr>
+								<th>Mes</th>
+								<th>Días</th>
+								<th>Horas</th>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach($shedulesMonth as $shedule)
 								<tr>
-									<th>Mes</th>
-									<th>Días</th>
-									<th>Horas</th>
+
+									<td>{{ $shedule->mes}}</td>
+									<td>{{ $shedule->Q_dias }}</td>
+									<td>{{ number_format((float)$shedule->Q_horas, 2, ',', '') }}</td>
+
 								</tr>
-							</thead>
-							<tbody>
-								@foreach($shedulesMonth as $shedule)
-									<tr>
+							@endforeach
+						</tbody>
+					</table>
+				</div>
 
-										<td>{{ $shedule->mes}}</td>
-										<td>{{ $shedule->Q_dias }}</td>
-										<td>{{ number_format((float)$shedule->Q_horas, 2, ',', '') }}</td>
+				@endif
 
-									</tr>
-								@endforeach
-							</tbody>
-						</table>
-					</div>
-
-        @endif
-
-      </div>
+		</div>
+	  @endif
     </div>
 
   </div><!--/.container-->
