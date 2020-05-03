5a92ce0830ce94b50a948019048297255937aa0f
Kendry Ortiz <ortizkendry95@gmail.com>
2020-05-03 13:09:35 -0500

    Pequeño cambio

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -32,16 +32,11 @@ class Kernel extends ConsoleKernel
             try {
                 DB::update("UPDATE stock SET costo_usd_modif=5 where consola='ps4' and costo_usd<=5");
 
-                echo "ACTUALIZACION EXITOSA DE JUEGOS PS4 QUE VALEN 5 USD O MENOS.\n";
-    
                 DB::update("UPDATE stock s LEFT JOIN 
                             (SELECT ID, IF(sugerido<(costo_usd * 0.25),FORMAT((costo_usd * 0.25),2),sugerido) as costo_usd_a_modificar FROM (SELECT ID, costo_usd, FORMAT(costo_usd * (0.02 + POWER(0.98,FLOOR((datediff(CURDATE(), Day))/7))),2) as sugerido FROM stock where consola='ps4' and costo_usd>5 and (titulo LIKE '%fifa%' or titulo LIKE '%pes$') ORDER BY ID ASC) AS primer_calculo) as rdo
                             ON s.ID = rdo.ID
                             SET s.costo_usd_modif = rdo.costo_usd_a_modificar
                             WHERE rdo.ID is not null");
-    
-                echo "ACTUALIZACION EXITOSA DE JUEGOS PS4 FIFA O PES QUE VALEN MAS DE 5 USD.\n";
-    
     
                 DB::update("UPDATE stock s LEFT JOIN 
                             (SELECT ID, IF(sugerido<(costo_usd * 0.25),FORMAT((costo_usd * 0.25),2),sugerido) as costo_usd_a_modificar FROM (SELECT ID, costo_usd, FORMAT(costo_usd * (0.015 + POWER(0.988,FLOOR((datediff(CURDATE(), Day))/7))),2) as sugerido FROM stock where consola='ps4' and costo_usd>5 and titulo NOT LIKE '%fifa%' and titulo NOT LIKE '%pes$' ORDER BY ID ASC) AS primer_calculo) as rdo
@@ -49,9 +44,11 @@ class Kernel extends ConsoleKernel
                             SET s.costo_usd_modif = rdo.costo_usd_a_modificar
                             WHERE rdo.ID is not null");
     
+                DB::commit();
+                echo "ACTUALIZACION EXITOSA DE JUEGOS PS4 QUE VALEN 5 USD O MENOS.\n";
+                echo "ACTUALIZACION EXITOSA DE JUEGOS PS4 FIFA O PES QUE VALEN MAS DE 5 USD.\n";
                 echo "ACTUALIZACION EXITOSA DE JUEGOS PS4 QUE NO SON FIFA O PES QUE VALEN MAS DE 5 USD.\n";
 
-                DB::commit();
             } catch (\Exception $e) {
                 DB::rollback();
                 echo "Error al ejecutar el crontab programado para domingos 5AM: " . $e->getMessage() . "\n";
