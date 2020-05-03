6092492fbc86bb503b5f867f3ff9460612df7e49
Kendry Ortiz <ortizkendry95@gmail.com>
2020-05-03 13:08:02 -0500

    Tarea programada nueva desde Laravel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -4,6 +4,7 @@ namespace App\Console;
 
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use DB;
 
 class Kernel extends ConsoleKernel
 {
@@ -24,8 +25,39 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+        $schedule->call(function () {
+            
+            DB::beginTransaction();
+
+            try {
+                DB::update("UPDATE stock SET costo_usd_modif=5 where consola='ps4' and costo_usd<=5");
+
+                echo "ACTUALIZACION EXITOSA DE JUEGOS PS4 QUE VALEN 5 USD O MENOS.\n";
+    
+                DB::update("UPDATE stock s LEFT JOIN 
+                            (SELECT ID, IF(sugerido<(costo_usd * 0.25),FORMAT((costo_usd * 0.25),2),sugerido) as costo_usd_a_modificar FROM (SELECT ID, costo_usd, FORMAT(costo_usd * (0.02 + POWER(0.98,FLOOR((datediff(CURDATE(), Day))/7))),2) as sugerido FROM stock where consola='ps4' and costo_usd>5 and (titulo LIKE '%fifa%' or titulo LIKE '%pes$') ORDER BY ID ASC) AS primer_calculo) as rdo
+                            ON s.ID = rdo.ID
+                            SET s.costo_usd_modif = rdo.costo_usd_a_modificar
+                            WHERE rdo.ID is not null");
+    
+                echo "ACTUALIZACION EXITOSA DE JUEGOS PS4 FIFA O PES QUE VALEN MAS DE 5 USD.\n";
+    
+    
+                DB::update("UPDATE stock s LEFT JOIN 
+                            (SELECT ID, IF(sugerido<(costo_usd * 0.25),FORMAT((costo_usd * 0.25),2),sugerido) as costo_usd_a_modificar FROM (SELECT ID, costo_usd, FORMAT(costo_usd * (0.015 + POWER(0.988,FLOOR((datediff(CURDATE(), Day))/7))),2) as sugerido FROM stock where consola='ps4' and costo_usd>5 and titulo NOT LIKE '%fifa%' and titulo NOT LIKE '%pes$' ORDER BY ID ASC) AS primer_calculo) as rdo
+                            ON s.ID = rdo.ID
+                            SET s.costo_usd_modif = rdo.costo_usd_a_modificar
+                            WHERE rdo.ID is not null");
+    
+                echo "ACTUALIZACION EXITOSA DE JUEGOS PS4 QUE NO SON FIFA O PES QUE VALEN MAS DE 5 USD.\n";
+
+                DB::commit();
+            } catch (\Exception $e) {
+                DB::rollback();
+                echo "Error al ejecutar el crontab programado para domingos 5AM: " . $e->getMessage() . "\n";
+            }
+
+        })->cron('0 5 * * 0');
     }
 
     /**
