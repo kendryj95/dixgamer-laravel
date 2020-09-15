f381c609b21af17a6839d69077e2967901562e16
Kendry Ortiz <ortizkendry95@gmail.com>
2020-09-15 16:24:22 -0500

    Pequeño cambio de texto en unos de los clipboards

--- resources/views/components/clipboards/account.blade.php
+++ resources/views/components/clipboards/account.blade.php
@@ -39,16 +39,21 @@
     </div>
 
     <div style="position: absolute; height: 100px; width: 100px;right: -50px; top: 50px;">
-					<span id="reactivar-copy{{$clientes_id}}" style="font-size:15px; background: white; font-weight: normal; color:#111;"><p>Hola {{ $nombre_cliente }}, por favor ingresá a nuestra cuenta/usuario con el nombre <b>{{ $account_name . " " . $account_surname }}</b> una vez más para RE ACTIVAR tu slot primario, una vez dentro de nuestro usuario:<br /><br />
+					<span id="reactivar-copy{{$clientes_id}}" style="font-size:15px; background: white; font-weight: normal; color:#111;"><p>Hola {{ $nombre_cliente }}, por favor ingresá a nuestra cuenta/usuario con el nombre <b>{{ $account_name . " " . $account_surname }}</b> una vez más para RE ACTIVAR tu slot primario. <br>
+
+						Una vez dentro de nuestro usuario:<br /><br />
 						1) Ir a Configuración > PSN/Administración de cuentas > Activar como tu PS4 principal > Activar<br />
 						2) Ir a Configuración > PSN/Administración de cuentas > Restaurar Licencias > Restaurar<br />
 						3) Reiniciar tu consola y acceder con tu usuario personal, recordá no volver a abrir nuestro usuario.<br /><br />
 
 						E-mail: {{$mail_fake}} <br>
 						Contraseña: <?php echo $pass;?><br /><br />
+
+						Saludos, <?php echo session()->get('usuario')->nombre_visible;?> de DixGamer.<br/><br>
+
                             <!-- Aprovecho para contarte que nuestros paVos de Fortnite bajaron de precio, <a href="https://dixgamer.com/categoria-producto/tarjetas/fortnite/">ver paVos baratos</a><br /><br /> -->
 						{!! html_entity_decode($oferta_fortnite) !!} <br>
-						Saludos, <?php echo session()->get('usuario')->nombre_visible;?> de DixGamer.<br/></p>
+						</p>
 					</span>
     </div>
 </div>
