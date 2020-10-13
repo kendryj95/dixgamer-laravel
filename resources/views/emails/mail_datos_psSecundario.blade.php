dfe7a8e9c75e0067038b6a556e6f13ed5855ac2b
Kendry Ortiz <ortizkendry95@gmail.com>
2020-10-13 15:10:50 -0500

    Ajustes en plantillas de emails

--- resources/views/emails/mail_datos_psSecundario.blade.php
+++ resources/views/emails/mail_datos_psSecundario.blade.php
@@ -288,28 +288,7 @@
 														<table width="100%" cellpadding="0" cellspacing="0">
 															<tr>
 																<td data-color="text" data-link-color="link text color" data-link-style="text-decoration:underline; color:#797c82;" class="aligncenter" style="font:12px/16px Arial, Helvetica, sans-serif; color:#797c82; padding:0 0 10px;">
-																	dixgamer.com, 2016. Todos los derechos reservados. <a style="text-decoration:underline; color:#797c82;"></a>
-																</td>
-															</tr>
-														</table>
-													</th>
-													<th class="thead" width="150" align="left" style="vertical-align:top; padding:0;">
-														<table class="center" align="right" cellpadding="0" cellspacing="0">
-															<tr>
-																<td class="btn" valign="top" style="line-height:0; padding:3px 0 0;">
-																	<a target="_blank" style="text-decoration:none;" href="#"><img src="file:///D|/Downloads/markup/images/ico-facebook.png" border="0" style="font:12px/15px Arial, Helvetica, sans-serif; color:#797c82;" align="left" vspace="0" hspace="0" width="6" height="13" alt="fb" /></a>
-																</td>
-																<td width="20"></td>
-																<td class="btn" valign="top" style="line-height:0; padding:3px 0 0;">
-																	<a target="_blank" style="text-decoration:none;" href="#"><img src="file:///D|/Downloads/markup/images/ico-twitter.png" border="0" style="font:12px/15px Arial, Helvetica, sans-serif; color:#797c82;" align="left" vspace="0" hspace="0" width="13" height="11" alt="tw" /></a>
-																</td>
-																<td width="19"></td>
-																<td class="btn" valign="top" style="line-height:0; padding:3px 0 0;">
-																	<a target="_blank" style="text-decoration:none;" href="#"><img src="file:///D|/Downloads/markup/images/ico-google-plus.png" border="0" style="font:12px/15px Arial, Helvetica, sans-serif; color:#797c82;" align="left" vspace="0" hspace="0" width="19" height="15" alt="g+" /></a>
-																</td>
-																<td width="20"></td>
-																<td class="btn" valign="top" style="line-height:0; padding:3px 0 0;">
-																	<a target="_blank" style="text-decoration:none;" href="#"><img src="file:///D|/Downloads/markup/images/ico-linkedin.png" border="0" style="font:12px/15px Arial, Helvetica, sans-serif; color:#797c82;" align="left" vspace="0" hspace="0" width="13" height="11" alt="in" /></a>
+																	dixgamer.com, {{date('Y')}}. Todos los derechos reservados. <a style="text-decoration:underline; color:#797c82;"></a>
 																</td>
 															</tr>
 														</table>
